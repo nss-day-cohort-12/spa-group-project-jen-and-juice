410998c627dfbee8254688c66008e96d06bef668
Michael Pam Bennette <KingofTenn@Michaels-MacBook-Air.local>
2016-02-15 13:30:41 -0600

    fixed the xhr request and iife

--- javascript/load.js
+++ javascript/load.js
@@ -1,28 +1,42 @@
 var Chatty= (function () {
 	var messageArray= [];
-
-	
-	
-
 	return {
 		loadMessages: function(callback){
-			var myRequest = new XMLHttpRequest();// Create an XHR object
-
-			myRequest.addEventListener("load", function () {
-				data = JSON.parse(this.responseText);
-				messageArray = data.messsage;
-				console.log(messageArray);
-
-				callback(message);
+			var loader = new XMLHttpRequest();// Create an XHR object
+			loader.addEventListener("load", function () {
+				// console.log(this.responseText);
+				var data = JSON.parse(this.responseText);
+				// console.log("data.categories: ", data.categories);
+				messageArray = data.message;
+				// console.log("messageArray", messageArray);
+				callback(messageArray);
 			});
-				
-		myRequest.open("GET", "json/msgs.json");
-		myRequest.send();
+		loader.open("GET", "json/msgs.json");
+		loader.send();
 		}
 	}
 })();
 
+function writeToDOM(callback) {
+	var output = document.getElementById("output"); 
+	var counter= 0;
+	console.log(callback);
+	callback.forEach(function(object){
+		var currentMessage = object.msg;
+		var msgString= "";
+		msgString += `<div id="messsage--${counter}" class="msg" ><p>${currentMessage}</p>`;
+		msgString += `<button id="button--${counter}" class="deleteButton">delete</button></div>`;
+		output.innerHTML+= msgString;	
+		counter ++;
+		});	
+}
+Chatty.loadMessages(writeToDOM);
 
-// function executeThisCodeAfterFileIsLoaded () {
-	
-// }
+
+
+
+
+
+
+
+
